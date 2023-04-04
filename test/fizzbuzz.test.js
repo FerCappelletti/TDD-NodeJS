d4c477922b7f2a87fdef01f836a3adc2dae3b1c9
FerCappelletti <fer_cappelletti@hotmail.com>
2023-04-04 12:28:44 -0300

    fizzbuzz test completed

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-
+import { fizzbuzz } from "../src/fizzbuzz.js";
 /*
 Write a function that when passing a number to it:
     - Display "fizz" if it is a multiple of 3
@@ -8,17 +8,11 @@ Write a function that when passing a number to it:
     - Display the number if it is not a multiple of any of the above.
 */
 
-
-const fizzbuzz = (number) => {
-        if (typeof number !== 'number') throw new Error('parameter should be a number')
-        if (Number.isNaN(number)) throw new Error('parameter should be a number')
-};
-
-
 describe("fizzbuzz", () => {
-    it('should be a function', () => {
-        expect(typeof fizzbuzz).toBe('function')
-    })
+    // This test is redundant   
+    // it('should be a function', () => {
+    //     expect(typeof fizzbuzz).toBe('function')
+    // })
 
     it('should throw if not number is provided as parameter' , () => {
         expect(() => fizzbuzz()).toThrow()
@@ -30,5 +24,21 @@ describe("fizzbuzz", () => {
 
     it('should throw a specific error messagge if not number is provided' , () => {
         expect(() => fizzbuzz(NaN)).toThrow('parameter should be a number')
+    })
+
+    it('should return 1 if number provided is 1' , () => {
+        expect(fizzbuzz(1)).toBe(1)
+    })
+
+    it('should return "fizz" if number provided is multiple of 3' , () => {
+        expect(fizzbuzz(12)).toBe("fizz")
+    })
+
+    it('should return "buzz" if number provided is multiple of 5' , () => {
+        expect(fizzbuzz(10)).toBe("buzz")
+    })
+
+    it('should return "fizzbuzz" if number provided is multiple of 3 & 5' , () => {
+        expect(fizzbuzz(15)).toBe("fizzbuzz")
     })
 })
